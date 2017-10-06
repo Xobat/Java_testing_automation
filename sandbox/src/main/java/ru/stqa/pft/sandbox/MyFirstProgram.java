98fe1bb78ea87ab63cf38f90fa0426f8990e6374
xobat <xobotok@mail.ru>
2017-10-07 01:50:51 +0300

    Созданы классы Square (Квадрат) и Rectangle (Прямоугольник), созданы конструкторы этих классов.

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,30 +3,28 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-
     hello("world");
     hello("user");
     hello("Alexey");
-    double l = 5;
 
-    double a = 4;
-    double b = 6;
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной "+ s.a +" = "+area(s));
 
-    System.out.println("Площадь прямоугольника со сторонами "+ a +" и " + b +" = "+area(a,b));
-        System.out.println("Площадь квадрата со стороной "+ l +" = "+area(l));
+    Rectangle a = new Rectangle(4,6);
+    System.out.println("Площадь прямоугольника со сторонами "+ a.a +" и " + a.b +" = "+area(a));
+
     }
-    public static  void hello(String somebody) {
 
+    public static  void hello(String somebody) {
         System.out.println("Hello, "+somebody+"!");
     }
 
-    public static double area (double len) {
-
-        return len*len;
+    public static double area (Square s) {
+        return s.a*s.a;
     }
 
-    public static double area (double a, double b) {
-        return a*b;
+    public static double area (Rectangle a) {
+        return a.a*a.b;
     }
 
 }
