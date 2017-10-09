de99c869dd90c3b5cd252975e14da3e6192a83c5
xobat <xobotok@mail.ru>
2017-10-09 23:56:11 +0300

    Добавил тест для проверки изменений уже созданной группы.

---
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/GroupModificationTest.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.adressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.adressbook.model.GroupFields;
+
+public class GroupModificationTest extends TestBase {
+
+  @Test
+  public void testGroupModification() {
+    app.getMoveManager().gotoGroups();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupFields(new GroupFields("test2", "test3", "tes4"));
+    app.getGroupHelper().submitGroupModified();
+  }
+
+}
